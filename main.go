924c5e1cdde72dd432a9013ab22a4bd66dce2402
phr3nzy <adilosama47@gmail.com>
2019-08-15 23:21:46 +0200

    Style: adhere to go code best practices

--- main.go
+++ main.go
@@ -18,7 +18,19 @@ func main() {
 	go func() {
 		t := time.Now()
 		genesisBlock := core.Block{}
-		genesisBlock = core.Block{Index: 0, Timestamp: t.String(), Vote: core.Vote{Candidate: "Osama", Voter: core.Voter{NationalID: "0000-0000-0000-0000", Timestamp: t.String()}}, Hash: core.CalculateHash(genesisBlock), PrevHash: ""}
+		genesisBlock = core.Block{
+			Index:     0,
+			Timestamp: t.String(),
+			Vote: core.Vote{
+				Candidate: "Osama",
+				Voter: core.Voter{
+					NationalID: "0000-0000-0000-0000",
+					Timestamp:  t.String(),
+				},
+			},
+			Hash:     core.CalculateHash(genesisBlock),
+			PrevHash: "",
+		}
 		spew.Dump(genesisBlock)
 
 		core.Mutex.Lock()

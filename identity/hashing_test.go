5b20e5b28b3ad8b756f554a44251b0af2fbde4b6
phr3nzy <adilosama47@gmail.com>
2019-08-17 11:51:31 +0200

    Edit: change test hashing logging formatting to error formatting

--- identity/hashing_test.go
+++ identity/hashing_test.go
@@ -19,7 +19,7 @@ func Test(t *testing.T) {
 		got := identity.Hash("Hello")
 		want := "53427dce55a4d75552a20f0deb0c42e1545b1411a6a979d0077f3fe26c3eba15"
 		if reflect.DeepEqual(got, want) != true {
-			t.Logf("got %v want %v", got, want)
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }

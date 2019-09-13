550d94d957078d18d789e8fb74d38f64770e536a
phr3nzy <adilosama47@gmail.com>
2019-09-13 12:32:08 +0200

    Add: errors package test

---
+++ errors/handlers_test.go
@@ -0,0 +1,15 @@
+package errors_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/phr3nzy/elections-blockchain/errors"
+)
+
+func Test(t *testing.T) {
+	t.Run("throw error and catch", func(t *testing.T) {
+		err := fmt.Errorf("Error!")
+		errors.HandleError(&err)
+	})
+}
